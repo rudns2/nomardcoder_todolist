9036d8d1072d49c7bac79ccdd0409805d6d5ce47
정겨운 <jeong-gyeoun@jeong-gyeoun-ui-MacBookAir.local>
2023-03-22 17:55:37 +0900

    todo 수정

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
 function deleteToDo(event){
  const li = event.target.parentElement;
+ li.remove();
 }
 
 function paintToDo(newTodo){
@@ -20,9 +26,11 @@ function paintToDo(newTodo){
 
 function handleToDoSubmit(event){
   event.preventDefault();
-  const newTodo = toDoInput.value;
+  const newTodo = toDoInput.value;``
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
