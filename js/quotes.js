31332272e45fd378572020b555c3c3ed0b6ce7e0
정겨운 <jeong-gyeoun@jeong-gyeoun-ui-MacBookAir.local>
2023-03-27 13:27:02 +0900

    styling

--- js/quotes.js
+++ js/quotes.js
@@ -46,5 +46,5 @@ const author = document.querySelector("#quote span:last-child");
 
 const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-quote.innerText = `${todayQuote.quote} - ` ;
-author.innerText = `${todayQuote.author} - `;
+quote.innerText = `${todayQuote.quote}` ;
+author.innerText = ` - ${todayQuote.author} - `;
